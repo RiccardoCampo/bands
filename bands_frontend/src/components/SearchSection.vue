8e8a64007e4d29c4fcbfb02710bd15ed916802e2
Riccardo Campo <riccardocampo@tutanota.com>
2024-09-10 20:40:04 +0200

    frontend: add search section

---
+++ bands_frontend/src/components/SearchSection.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="search">
+    <input class="search"  v-model="text" placeholder="type an artist...">
+    <button class="search" @click="$emit('searchClicked')"><search-icon height=31 width=31 iconColor="inherit"></search-icon></button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SearchSection',
+  data () {
+    return {
+      text: ""
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+div.search {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+input.search {
+  background-color: var(--white);
+  width: 500px;
+  height: 28px;
+  outline: none;
+  border-width: 3px;
+  border-color: var(--darkred);
+  border-style: solid;
+  font-size: 1.3pc;
+  color: var(--grey);
+  transition: all 0.2s;
+}
+
+input.search:focus {
+  border-color: var(--darkyellow);
+}
+
+button.search {
+  background-color: var(--darkred);
+  stroke: var(--darkyellow);
+  border: none;
+  transition: all 0.1s;
+}
+
+button.search:active {
+  background-color: var(--darkyellow);
+  stroke: var(--darkred);
+}
+
+</style>
