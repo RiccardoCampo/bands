99ff3887139cabb0b24b3d6d794b29a26b04e43b
Riccardo Campo <riccardocampo@tutanota.com>
2024-09-10 20:37:17 +0200

    frontend: add icons

---
+++ bands_frontend/src/components/icons/SearchIcon.vue
@@ -0,0 +1,34 @@
+<template>
+    <svg-icon :iconName="iconName" :width="width" :height="height" :iconColor="iconColor">
+        <circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+    </svg-icon>
+</template>
+<script>
+
+import SvgIcon from './SvgIcon.vue';
+
+export default {
+  props: {
+      iconName: {
+          type: String,
+          default: 'box'
+      },
+      width: {
+          type: [Number, String],
+          default: 18
+      },
+      height: {
+          type: [Number, String],
+          default: 18
+      },
+      iconColor: {
+          type: String,
+          default: 'currentColor'
+      }
+  },
+  components: {
+    'svg-icon': SvgIcon,
+  },
+}
+
+</script>
