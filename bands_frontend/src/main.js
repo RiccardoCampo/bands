87c53d8166f6719fe01f41b04ec3b8c0e18402b1
Riccardo Campo <riccardocampo@tutanota.com>
2024-10-07 08:51:52 +0200

    icons: add more icons

--- bands_frontend/src/main.js
+++ bands_frontend/src/main.js
@@ -3,9 +3,19 @@ import App from './App.vue'
 
 import '../assets/css/main.css';
 import SearchIcon from './components/icons/SearchIcon.vue';
+import StarIcon from './components/icons/StarIcon.vue';
+import StarFilledIcon from './components/icons/StarFilledIcon.vue';
+import CheckIcon from './components/icons/CheckIcon.vue';
+import CrossIcon from './components/icons/CrossIcon.vue';
+import ExternalLink from './components/icons/ExternalLink.vue';
 
 const app = createApp(App)
 
 app.component('search-icon', SearchIcon)
+app.component('star-icon', StarIcon)
+app.component('star-filled-icon', StarFilledIcon)
+app.component('check-icon', CheckIcon)
+app.component('cross-icon', CrossIcon)
+app.component('external-link', ExternalLink)
 
 app.mount('#app')
