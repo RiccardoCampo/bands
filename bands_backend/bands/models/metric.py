60c371dae73a1bd8c0ce406d188b36304aa62cbe
Riccardo Campo <riccardocampo@tutanota.com>
2024-10-12 18:36:39 +0200

    models: metric, add category field

--- bands_backend/bands/models/metric.py
+++ bands_backend/bands/models/metric.py
@@ -17,9 +17,20 @@ class Metric(SoftDeleteModel):
         FLAG = 0, gettext_lazy("flag")
         VALUE = 1, gettext_lazy("value")
 
+    class MetricCategory(models.IntegerChoices):
+        """
+        Metric type enum.
+        """
+
+        SCORE = 0, gettext_lazy("score")
+        MAIN_SCORE = 1, gettext_lazy("main_score")
+        GENRE = 2, gettext_lazy("genre")
+        SUBGENRE = 3, gettext_lazy("subgenre")
+
     id = PositiveAutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
     type = PositiveTinyIntegerField(choices=MetricType)
+    category = PositiveTinyIntegerField(choices=MetricCategory)
 
     class Meta:
         """
