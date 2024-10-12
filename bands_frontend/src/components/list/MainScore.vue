360d3775c1389ec51860288b883f561e40034c1c
Riccardo Campo <riccardocampo@tutanota.com>
2024-10-12 19:35:17 +0200

    components: add list

---
+++ bands_frontend/src/components/list/MainScore.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="width: 200px; justify-self: right;">
+        <star-filled-icon v-for="index in score" :key="index" :height="24" :width="24" :iconColor="color"></star-filled-icon>
+        <star-icon v-for="index in (5 - score)" :key="index" :height="24" :width="24" :iconColor="color"></star-icon>
+    </div>
+</template>
+  
+<script>
+
+export default {
+  name: 'MainScore',
+  props: {
+      score: Number,
+      color: String
+  },
+}
+
+</script>
---
+++ bands_frontend/src/mixins/ColorsMixin.vue
@@ -0,0 +1,20 @@
+<script>
+import { ColorOrder } from '@/config';
+
+
+export default {
+name: 'ColorsMixin',
+methods: {
+    getColor (index) {
+        return ColorOrder[Math.floor(index) % ColorOrder.length];
+    },
+    addColors (array) {
+        const offset = Math.floor(Math.random() * ColorOrder.length);
+        
+        return array.map((element, index) => ({...element, color: this.getColor(index + offset)}))
+    },
+}
+}
+
+</script>
+  
