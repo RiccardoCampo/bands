39e857acdafffa23d00b582dc9274592dfceacb1
Riccardo Campo <riccardocampo@tutanota.com>
2024-12-05 22:06:16 +0100

    services/state: update artist repository and state

--- bands_frontend/src/services/API/ArtistsAPIRepository.js
+++ bands_frontend/src/services/API/ArtistsAPIRepository.js
@@ -1,14 +1,23 @@
 import axios from 'axios'
 
-const BASE_URL = `${process.env.VUE_APP_BANDS_API_URL}/artist`
+const BASE_URL = `${process.env.VUE_APP_BANDS_API_URL}/artist/`
 
 export default {
-  async index (page, name) {
+  async index (page, name, metrics) {
     let queryString = `page=${page}`
 
-    if (name) {
+    if (name)
       queryString += `&name=${name}`
-    }
+
+    if (metrics)
+      metrics.forEach(metric => {
+        if (metric.type == "value")
+          queryString += `&${metric.name}=[${metric.value[0]},${metric.value[1]}]`
+        else
+          queryString += `&${metric.name}=${metric.value}`
+      });
+
+    console.log(queryString)
 
     return axios.get(`${BASE_URL}?${queryString}`)
   }
