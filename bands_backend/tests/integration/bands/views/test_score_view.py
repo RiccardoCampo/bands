a1680809442660d4cd185f4fe654d063161db86e
riccardo <riccardocampo@tutanota.com>
2025-07-13 19:28:18 +0200

    tests: test score upsert endpoint

--- bands_backend/tests/integration/bands/views/test_score_view.py
+++ bands_backend/tests/integration/bands/views/test_score_view.py
@@ -146,3 +146,96 @@ class TestScoreViewSet(TestCase):
         self.assertEqual(response.status_code, 404)
         self.assertEqual(json.loads(response.content), {"not_found_error": "Score matching query does not exist."})
         self.assertEqual(Score.objects.count(), len(SCORES))
+
+    def test_bulk_upsert_valid(self) -> None:
+        """
+        Assert that the test scores are created or updated correctly.
+        """
+
+        test_score_1_id = 1
+        test_score_1_new_value = 3
+        test_score_2_id = 2
+        test_score_2_new_value = 4
+        test_score_updates = {
+            "updates": [
+                {"id": test_score_1_id, "value": test_score_1_new_value},
+                {"id": test_score_2_id, "value": test_score_2_new_value},
+                {"artist_id": 2, "metric_id": 2, "value": 2},
+            ]
+        }
+
+        response = self.client.put(
+            "/bands/score/bulk-upsert/", data=test_score_updates, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Score.objects.count(), len(SCORES) + 1)
+
+        self.assertEqual(Score.objects.get(id=test_score_1_id).value, test_score_1_new_value)
+        self.assertEqual(Score.objects.get(id=test_score_2_id).value, test_score_2_new_value)
+
+    def test_bulk_upsert_invalid(self) -> None:
+        """
+        Return a bad response if the input payload is invalid.
+        """
+
+        for payload in [
+            {
+                "updates": [
+                    {"value": 2},
+                ]
+            },
+            {
+                "updates": [
+                    {
+                        "id": 1,
+                    },
+                ]
+            },
+            {},
+            {
+                "updates": [
+                    {"artist_id": 1, "value": 2},
+                ]
+            },
+            {
+                "updates": [
+                    {"metric_id": 1, "value": 2},
+                ]
+            },
+        ]:
+            response = self.client.put("/bands/score/bulk-upsert/", data=payload, content_type="application/json")
+
+            self.assertEqual(response.status_code, 400)
+
+    def test_bulk_upsert_not_found(self) -> None:
+        """
+        Return 404 if one of the ids is missing. Successful updates are rolled back.
+        """
+
+        response = self.client.put(
+            f"/bands/score/{len(SCORES) + 1}/", data={"value": 5}, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(json.loads(response.content), {"not_found_error": "Score matching query does not exist."})
+
+        test_score_1_id = 1
+        test_score_1_new_value = 3
+        test_score_2_id = 20000
+        test_score_2_new_value = 4
+        test_score_updates = {
+            "updates": [
+                {"id": test_score_1_id, "value": test_score_1_new_value},
+                {"id": test_score_2_id, "value": test_score_2_new_value},
+            ]
+        }
+
+        response = self.client.put(
+            "/bands/score/bulk-upsert/", data=test_score_updates, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(json.loads(response.content), {"not_found_error": "Score matching query does not exist."})
+
+        self.assertNotEqual(Score.objects.get(id=test_score_1_id).value, test_score_1_new_value)
